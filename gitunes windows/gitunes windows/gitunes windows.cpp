245fda81c9c2be6e6fc679bec36506090b836373
Will Eccles <willeccles860+github@gmail.com>
2016-01-04 15:40:10 -0500

    Created VS project.

---
+++ gitunes windows/gitunes windows/gitunes windows.cpp
@@ -0,0 +1,15 @@
+// gitunes windows.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[], char *envp[])
+{
+	// parse args
+
+    return 0;
+}
+
